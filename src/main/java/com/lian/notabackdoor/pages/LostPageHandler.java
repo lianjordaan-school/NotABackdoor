0e592cf08ec32e624834d8319c34c719b66d7743
LianJordaan <lianjordaan30052005@gmail.com>
2023-04-16 18:42:21 +0200

    Moved file creation to Utils.java

--- src/main/java/com/lian/notabackdoor/pages/LostPageHandler.java
+++ src/main/java/com/lian/notabackdoor/pages/LostPageHandler.java
@@ -31,31 +31,8 @@ public class LostPageHandler implements HttpHandler {
 
     }
     public void DisplayLostPage(HttpExchange exchange) throws IOException {
-        new File(NotABackdoor.getPlugin(NotABackdoor.class).getDataFolder(), "pages/").mkdirs();
+        Utils.generateFiles();
         File file = new File(NotABackdoor.getPlugin(NotABackdoor.class).getDataFolder(), "pages/lost.html");
-
-        if (!file.exists()) {
-            String content = null;
-            // Create the lost page file if it doesn't exist
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("pages/lost.html");
-            //NotABackdoor.getPlugin(NotABackdoor.class).getLogger().log(Level.INFO, inputStream.toString());
-            if (inputStream != null) {
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                int nRead;
-                byte[] data = new byte[1024];
-                while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                    buffer.write(data, 0, nRead);
-                }
-                buffer.flush();
-                content = new String(buffer.toByteArray(), StandardCharsets.UTF_8);
-                inputStream.close();
-            } else {
-                content = "<html><body><h1>404 - Not Found</h1></body></html>";
-            }
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.write(content.getBytes());
-            fileOutputStream.close();
-        }
 
         String response = new String(Files.readAllBytes(file.toPath()));
         exchange.sendResponseHeaders(404, response.length());
