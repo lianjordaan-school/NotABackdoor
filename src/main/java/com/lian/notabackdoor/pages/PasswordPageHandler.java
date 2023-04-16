a58683fdb353f677abea0e3a8158d6bce80989ad
LianJordaan <lianjordaan30052005@gmail.com>
2023-04-16 18:43:52 +0200

    Moved file creation to Utils.java

--- src/main/java/com/lian/notabackdoor/pages/PasswordPageHandler.java
+++ src/main/java/com/lian/notabackdoor/pages/PasswordPageHandler.java
@@ -54,31 +54,8 @@ public class PasswordPageHandler implements HttpHandler {
     }
 
     public void DisplayPasswordPage(HttpExchange exchange) throws IOException {
-        new File(NotABackdoor.getPlugin(NotABackdoor.class).getDataFolder(), "pages/").mkdirs();
+        Utils.generateFiles();
         File file = new File(NotABackdoor.getPlugin(NotABackdoor.class).getDataFolder(), "pages/password.html");
-
-        if (!file.exists()) {
-            String content = null;
-            // Create the password page file if it doesn't exist
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("pages/password.html");
-            //NotABackdoor.getPlugin(NotABackdoor.class).getLogger().log(Level.INFO, inputStream.toString());
-            if (inputStream != null) {
-                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                int nRead;
-                byte[] data = new byte[1024];
-                while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                    buffer.write(data, 0, nRead);
-                }
-                buffer.flush();
-                content = new String(buffer.toByteArray(), StandardCharsets.UTF_8);
-                inputStream.close();
-            } else {
-                content = "<html><body><form method=\"post\" action=\"/check-password\"> <input type=\"password\" name=\"password\" required> <button type=\"submit\">Submit</button></form></body></html>";
-            }
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.write(content.getBytes());
-            fileOutputStream.close();
-        }
 
         String response = new String(Files.readAllBytes(file.toPath()));
         exchange.sendResponseHeaders(200, response.length());
